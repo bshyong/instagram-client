e6b6cde10c14d7d5fccd98bf0c672795a2478165
Ben <hello@benshyong.com>
2015-02-05 17:57:29 -0800

    initial commit

---
+++ instagram-reader/instagram-reader/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  instagram-reader
+//
+//  Created by Ben on 2/5/15.
+//  Copyright (c) 2015 Common Sense Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
